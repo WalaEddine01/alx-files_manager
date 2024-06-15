8ab4639ca109f0e881945f8671dd0835e401aed0
walaeddine01 <walaaeddine33@gmail.com>
2024-06-15 22:38:42 +0100

    I made the changes on 2024-06-15 22:38:42

--- utils/redis.js
+++ utils/redis.js
@@ -2,15 +2,13 @@ import { createClient } from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
+
   constructor() {
-    this.client = createClient().on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error}`);
-    });
+    this.client = createClient();
 
     this.getAsync = promisify(this.client.get).bind(this.client);
     this.setAsync = promisify(this.client.set).bind(this.client);
     this.delAsync = promisify(this.client.del).bind(this.client);
-    this.expireAsync = promisify(this.client.expire).bind(this.client);
   }
 
   isAlive() {
@@ -18,29 +16,16 @@ class RedisClient {
   }
 
   async get(key) {
-    try {
-      return await this.getAsync(key);
-    } catch (error) {
-      console.error(`Error getting key ${key}: ${error}`);
-      return null;
-    }
+    return await this.getAsync(key);
   }
 
   async set(key, value, duration) {
-    try {
-      await this.setAsync(key, value);
-      await this.expireAsync(key, duration);
-    } catch (error) {
-      console.error(`Error setting key ${key} with value ${value} and duration ${duration}: ${error}`);
-    }
+    await this.setAsync(key, value);
+    this.client.expire(key, duration);
   }
 
   async del(key) {
-    try {
-      await this.delAsync(key);
-    } catch (error) {
-      console.error(`Error deleting key ${key}: ${error}`);
-    }
+    await this.delAsync(key);
   }
 }
 
