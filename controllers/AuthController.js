7a5feab460f4a3df47fb8a75504dfb71b80ee30b
walaeddine01 <walaaeddine33@gmail.com>
2024-06-20 18:55:21 +0100

    I made the changes on 2024-06-20 18:55:21

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 import { v4 as uuidv4 } from 'uuid';
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
@@ -19,9 +20,49 @@ const getConnect = async (req, res) => {
     return res.status(401).send({ error: 'Unauthorized' });
   }
   const token = uuidv4();
-  await redisClient.set('token', token, 86400);
+  const key = `auth_${token}`;
+  const userID = a._id;
+  const userIDstring = userID.toString();
+  await redisClient.set(key, userIDstring, 86400);
 
   return res.status(200).send({ token });
 };
 
-export default getConnect;
+const getDisconnect = async (req, res) => {
+  const token = req.headers['x-token'];
+  const key = `auth_${token}`;
+  const userID = await redisClient.get(key);
+  if (userID) {
+    await redisClient.del(key);
+    return res.status(204).send();
+  }
+  return res.status(401).send({ error: 'Unauthorized' });
+};
+
+const getMe = async (req, res) => {
+  const token = req.headers['x-token'];
+  const key = `auth_${token}`;
+  const userID = await redisClient.get(key);
+
+  if (!userID) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  const c = dbClient.db.collection('users');
+  let user;
+  try {
+    user = await c.findOne({ _id: new ObjectId(userID) });
+
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).send({ id: user._id, email: user.email });
+  } catch (error) {
+    console.error('Error retrieving user:', error);
+    return res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
+export {
+  getConnect, getDisconnect, getMe,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { stats, status } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
-import getConnect from '../controllers/AuthController';
+import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
 
 const router = Router();
 router.get('/status', status);
@@ -11,5 +11,7 @@ router.get('/stats', stats);
 router.post('/users', postNew);
 
 router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('/users/me', getMe);
 
 export default router;
