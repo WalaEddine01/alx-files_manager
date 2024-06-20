7c455efaf347d1428d55bd358ae5100c524f605b
walaeddine01 <walaaeddine33@gmail.com>
2024-06-20 23:14:18 +0100

    I made the changes on 2024-06-20 23:14:18

---
+++ controllers/FilesController.js
@@ -0,0 +1,67 @@
+import { ObjectId } from 'mongodb';
+import path from 'path';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+const FOLDER_PATH = process.env.FOLDER_PATH || '/tmp/files_manager';
+
+const postUpload = async (req, res) => {
+  const token = req.headers['x-token'];
+  const key = `auth_${token}`;
+  const {
+    name, type, parentId = '0', isPublic = false, data,
+  } = req.body;
+
+  const userID = await redisClient.get(key);
+
+  if (!userID) {
+    return res.status(401).send({ error: 'Unauthorized' });
+  }
+  if (!name) return res.status(400).send({ error: 'Missing name' });
+  if (!type) return res.status(400).send({ error: 'Missing type' });
+  if (!data && type !== 'folder') return res.status(400).send({ error: 'Missing data' });
+  const c = dbClient.db.collection('users');
+  let user;
+  try {
+    user = await c.findOne({ _id: new ObjectId(userID) });
+
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    const f = dbClient.db.collection('files');
+    if (parentId !== '0') {
+      const parentFile = await f.findOne({ _id: new ObjectId(parentId) });
+
+      if (!parentFile) return res.status(400).send({ error: 'Parent not found' });
+      if (parentFile.type !== 'folder') return res.status(400).send({ error: 'Parent is not a folder' });
+    }
+    const fileData = {
+      userId: new ObjectId(userID),
+      name,
+      type,
+      isPublic,
+      parentId: parentId === '0' ? '0' : new ObjectId(parentId),
+    };
+    if (type !== 'folder') {
+      if (!fs.existsSync(FOLDER_PATH)) fs.mkdirSync(FOLDER_PATH, { recursive: true });
+      const localPath = path.join(FOLDER_PATH, uuidv4());
+      fs.writeFileSync(localPath, Buffer.from(data, 'base64'));
+    }
+    const result = await c.insertOne(fileData);
+    return res.status(201).send({
+      id: result.insertedId,
+      userId: fileData.userId,
+      name: fileData.name,
+      type: fileData.type,
+      isPublic: fileData.isPublic,
+      parentId: fileData.parentId,
+    });
+  } catch (error) {
+    console.error('Error retrieving user:', error);
+    return res.status(500).send({ error: 'Internal Server Error' });
+  }
+};
+
+export default postUpload;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { stats, status } from '../controllers/AppController';
 import postNew from '../controllers/UsersController';
 import { getConnect, getDisconnect, getMe } from '../controllers/AuthController';
+import postUpload from '../controllers/FilesController';
 
 const router = Router();
 router.get('/status', status);
@@ -13,5 +14,6 @@ router.post('/users', postNew);
 router.get('/connect', getConnect);
 router.get('/disconnect', getDisconnect);
 router.get('/users/me', getMe);
+router.post('/files', postUpload);
 
 export default router;
